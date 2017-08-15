b3ed6d12218003905fb400f00183e3fcbbe1636a
Mark Janes <mark.a.janes@intel.com>
2017-08-15 11:55:07 -0700

    perf: add driver2 alu2 workloads

---
+++ alu2_o_windowed/build.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import os
+import sys
+import os.path as path
+sys.path.append(path.join(path.dirname(path.abspath(sys.argv[0])), "..", "repos", "mesa_ci"))
+import build_support as bs
+
+
+bs.build(bs.PerfBuilder("alu2_o", iterations=5, windowed=True))
+
